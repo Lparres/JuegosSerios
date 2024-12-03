3b368c335e33ea85e233fbc2aa6fdb7eb882541b
Vivu24 <javivu2.12004@gmail.com>
2024-12-03 15:26:37 +0100

    novelnavmesh

---
+++ Proyecto Correa/Assets/Scripts/NPC.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NPC : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    public float wanderRadius = 10f;
+    public float wanderTimer = 5f;
+
+    private float timer;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        timer = wanderTimer;
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= wanderTimer)
+        {
+            Vector3 newPos = RandomNavSphere(transform.position, wanderRadius, -1);
+            agent.SetDestination(newPos);
+            timer = 0;
+        }
+    }
+    public static Vector3 RandomNavSphere(Vector3 origin, float dist, int layerMask)
+    {
+        Vector3 randomDirection = Random.insideUnitSphere * dist;
+        randomDirection += origin;
+
+        NavMeshHit navHit;
+        NavMesh.SamplePosition(randomDirection, out navHit, dist, layerMask);
+
+        return navHit.position;
+    }
+}
